c5b80391ff442b239005f325172cfe584e5beda6
Robert Dahmer <robertcontato01@gmail.com>
2020-07-20 19:25:18 -0300

    Mais estudos :)

--- Fundamentos/looping.js
+++ Fundamentos/looping.js
@@ -1,12 +1,16 @@
+/*var horário = 0
+for (; true; horário++) {
+    console.log(horário)
+}*/
+
+
 function sleep(delay) {
     var start = new Date().getTime();
     while (new Date().getTime() < start + delay);
 }
-
 
 var andarDesejado = 5
 for (var elevador = 1; elevador != andarDesejado +1 || elevador < andarDesejado; elevador = elevador + 1) {
     sleep(2000)
     var AndarAtual = console.log(`Você está no andar ${elevador}`)
 }
-
