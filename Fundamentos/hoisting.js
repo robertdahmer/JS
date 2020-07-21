d31be32dcf6ff182e3466e792c7268fe2f13a506
Robert Dahmer <robertcontato01@gmail.com>
2020-07-20 21:42:18 -0300

    Mais estudos :)

---
+++ Fundamentos/hoisting.js
@@ -0,0 +1,14 @@
+function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+  
+  const car1 = new Car('Eagle', 'Talon TSi', 1993);
+  
+  console.log(car1.make);
+
+
+  function Games (ano, nome, console) {
+      
+  }
