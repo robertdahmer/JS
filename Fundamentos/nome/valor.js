85197a139263fed061fcf545da2c0a144764f392
Robert Dahmer <robertcontato01@gmail.com>
2020-07-20 22:08:02 -0300

    Mais estudos :)

---
+++ Fundamentos/nome/valor.js
@@ -0,0 +1,13 @@
+const cliente = {
+    nome: 'Robert',
+    idade: 12,
+    peso: 75,
+    endereço: {
+        nome: 'Ivone Maria Pires 22',
+        bairro: 'Centro',
+        cidade: 'Balneário Piçarras'
+    }
+
+}
+
+console.log(cliente.endereço.cidade)
