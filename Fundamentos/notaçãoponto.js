8e809981e173f31679de15de8e9d90511af1da5f
Robert Dahmer <robertcontato01@gmail.com>
2020-07-22 17:14:22 -0300

    Mais estudos :)

--- Fundamentos/notaçãoponto.js
+++ Fundamentos/notaçãoponto.js
@@ -10,7 +10,11 @@ let pessoa = {
 let {idade, nome} = pessoa
 let {logradouro, numero} = pessoa.endereço
 let {sobrenome = 'Dahmer', tamanhoDoPau = '17cm'} = pessoa
-console.log(idade, nome, logradouro, numero)
-console.log(`Oi meu nome é ${nome} ${sobrenome}! Tenho ${idade} anos e ${tamanhoDoPau} de pau. Moro na ${logradouro}, que tem o número ${numero}`)
+/*console.log(idade, nome, logradouro, numero)
+console.log(`Oi meu nome é ${nome} ${sobrenome}! Tenho ${idade} anos e ${tamanhoDoPau} de pau. Moro na ${logradouro}, que tem o número ${numero}`)*/
 
-console.log(pessoa)
+let {nome: n, idade: i} = pessoa
+console.log(i, n)
+if (pessoa.nome === n || n === nome) {
+    console.log('É identico essas porra')
+}
