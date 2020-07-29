2ba123eb9dd574a24059cfa0fae8fd7c3a97f24c
Robert Dahmer <robertcontato01@gmail.com>
2020-07-28 21:33:42 -0300

    Mais estudos :)

---
+++ Fundamentos/switch.js
@@ -0,0 +1,11 @@
+function qualfoiPorra(nota) {
+        switch (nota) {
+            case 10:
+            case 9:
+                console.log('Você tirou 10, seu merdinha!')
+                
+        }
+}
+
+
+qualfoiPorra(10)
