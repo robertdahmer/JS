602b81082188a56ea37bb8477a1b21c220b481ef
Robert Dahmer <robertcontato01@gmail.com>
2020-08-04 20:51:35 -0300

    Mais estudos :)

--- Funções/testeFunção.js
+++ Funções/testeFunção.js
@@ -8,3 +8,13 @@ function print(texto) {
 }
 
 print('Olá, Mundo!')
+
+const radius = function (altura, largura) {
+    const area = altura * largura
+    if (area > 20) {
+        console.log(`Área (${area}) com o tamanho maior que o permitido (20)`)
+        console.log(area)
+    }
+}
+
+radius('5', 5, 2, 2, 2) // ignora os parâmetros depois do '5'
