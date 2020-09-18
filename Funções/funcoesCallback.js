674f5a325ca611e6ba5155c86bd748dfda1f1918
Robert Dahmer <robertcontato01@gmail.com>
2020-09-17 22:40:14 -0300

    Mais estudos :)

--- Funções/funcoesCallback.js
+++ Funções/funcoesCallback.js
@@ -1,3 +1,10 @@
-const Fabricante = ['Mercedes', 'BMW', 'Chevrolet']
+numeros  = [0,1,2,3,4,5,6,7,8,9]
+total = 0
 
-function imprimit()
+function somar(item, indice, array) {
+        total += item;
+        array[indice] = total
+}
+
+numeros.forEach(somar)
+console.log(numeros)
